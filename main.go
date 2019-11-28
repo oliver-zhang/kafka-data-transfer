160e3848d7a83830378be087b6623b83e3fe6365
oliver zhang <oliver.zjf@gmail.com>
2019-11-28 17:48:40 +0800

    init

--- main.go
+++ main.go
@@ -1,37 +1,23 @@
 package main
 
 import (
-	"github.com/kafka_data_transfer/transfer"
+	"github.com/kafka-data-transfer/transfer"
 	"gopkg.in/alecthomas/kingpin.v2"
-	"golang.org/x/net/proxy"
 )
 
 var (
-	brokenList = kingpin.Flag("brokenList","List of brokers to connect").Strings()
-	topic = kingpin.Flag("topic","the name of transfer topic").String()
+	brokenList = kingpin.Flag("brokenList", "List of brokers to connect").Strings()
+	topic      = kingpin.Flag("topic", "the name of transfer topic").String()
 	maxRetry   = kingpin.Flag("maxRetry", "Retry limit").Default("5").Int()
 )
 
-
 func main() {
 	kingpin.Parse()
-	producer := transfer.InitKafkaProducer(*maxRetry,*brokenList)
-	transfer.SyncMsg(*brokenList,*topic);
+	producer := transfer.InitKafkaProducer(*maxRetry, *brokenList)
+	transfer.SyncMsg(*brokenList, *topic)
 	defer func() {
-		if err := producer.Close(); err!=nil {
+		if err := producer.Close(); err != nil {
 			panic(err)
 		}
 	}()
 }
-
-
-
-
-
-
-
-
-
-
-
-
